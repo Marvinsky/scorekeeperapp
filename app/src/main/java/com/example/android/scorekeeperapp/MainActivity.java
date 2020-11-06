cb9c1c2d82671c76939f0b67d683813d9f8cf435
Marvinsky <marvin.abisrror@gmail.com>
2020-11-06 10:57:58 -0500

    add title

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.scorekeeperapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
     int foulsTeamA = 0;
     int offsidesTeamA = 0;
     int cornersTeamA = 0;
-    int possesionTeamA = 0;
+
+    int posessionTeamA = 0;
+    int posessionTeamB = 0;
+    int posessionTeam = 0;
 
     int goalsTeamB = 0;
     int yellowCardsTeamB = 0;
@@ -22,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
     int foulsTeamB = 0;
     int offsidesTeamB = 0;
     int cornersTeamB = 0;
-    int possesionTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            System.exit(0);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     private void displayGoalsForTeamA(int goals) {
         TextView goalsView  = (TextView) findViewById(R.id.team_a_goals);
@@ -61,17 +83,6 @@ public class MainActivity extends AppCompatActivity {
         cornersView.setText(String.valueOf(corners));
     }
 
-    private void displayPossesionA(int possesion) {
-        TextView possesionView = (TextView) findViewById(R.id.team_a_possession);
-        possesionView.setText(possesionTeamA() + "%");
-    }
-
-    private String possesionTeamA() {
-        return String.valueOf(Math.round(possesionTeamA/(possesionTeamA + possesionTeamB)));
-    }
-
-
-
     private void displayGoalsForTeamB(int goals) {
         TextView goalsView  = (TextView) findViewById(R.id.team_b_goals);
         goalsView.setText(String.valueOf(goals));
@@ -100,15 +111,6 @@ public class MainActivity extends AppCompatActivity {
     private void displayCornersB(int corners) {
         TextView cornersView = (TextView) findViewById(R.id.team_b_corners);
         cornersView.setText(String.valueOf(corners));
-    }
-
-    private void displayPossesionB(int possesion) {
-        TextView possesionView = (TextView) findViewById(R.id.team_b_possession);
-        possesionView.setText(possesionTeamB() + "%");
-    }
-
-    private String possesionTeamB() {
-        return String.valueOf(Math.round(possesionTeamB/(possesionTeamA + possesionTeamB)));
     }
 
     public void addGoalForTeamA(View view) {
@@ -142,12 +144,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addPosessionForTeamA(View view) {
-        possesionTeamA += 1;
-        displayPossesionA(possesionTeamA);
+        TextView textViewA = (TextView)findViewById(R.id.team_a_possession);
+        String numberLiteral = textViewA.getText().toString().split(" ")[0];
+        posessionTeamA = Integer.valueOf(numberLiteral).intValue();
+
+        posessionTeamA += 1;
+
+        if (posessionTeamA > 99) {
+            posessionTeamA = 100;
+        }
+
+        displayPossesion(posessionTeamA, true);
     }
-
-
-
 
     public void addGoalForTeamB(View view) {
         goalsTeamB += 1;
@@ -180,8 +188,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addPosessionForTeamB(View view) {
-        possesionTeamB += 1;
-        displayPossesionB(possesionTeamB);
+        TextView textViewB = (TextView)findViewById(R.id.team_b_possession);
+        String numberLiteral = textViewB.getText().toString().split(" ")[0];
+        posessionTeamB = Integer.valueOf(numberLiteral).intValue();
+
+        posessionTeamB += 1;
+
+        if (posessionTeamB > 99) {
+            posessionTeamB = 100;
+        }
+
+        displayPossesion(posessionTeamB, false);
+    }
+
+    private void displayPossesion(int possesion, boolean isFlagA) {
+        TextView possesionViewA = (TextView) findViewById(R.id.team_a_possession);
+        TextView possesionViewB = (TextView) findViewById(R.id.team_b_possession);
+
+
+        if (isFlagA) {
+            possesionViewA.setText(possesion + " %");
+            possesionViewB.setText(String.valueOf(100 - possesion) + " %");
+        } else {
+            possesionViewA.setText(String.valueOf(100 - possesion) + " %");
+            possesionViewB.setText(String.valueOf(possesion) + " %");
+        }
+    }
+
+    private void setPossesionTeamZero() {
+        TextView possesionViewA = (TextView) findViewById(R.id.team_a_possession);
+        TextView possesionViewB = (TextView) findViewById(R.id.team_b_possession);
+        possesionViewA.setText(String.valueOf(0));
+        possesionViewB.setText(String.valueOf(0));
     }
 
     public void resetPlayers(View view) {
@@ -191,12 +229,27 @@ public class MainActivity extends AppCompatActivity {
         yellowCardsTeamB = 0;
         redCardsTeamA = 0;
         redCardsTeamB = 0;
+        foulsTeamA = 0;
+        offsidesTeamA = 0;
+        cornersTeamA = 0;
+        posessionTeam = 0;
+        foulsTeamB = 0;
+        offsidesTeamB = 0;
+        cornersTeamB = 0;
+
         displayGoalsForTeamA(goalsTeamA);
         displayYellowCardsForTeamA(yellowCardsTeamA);
         displayRedCardsForTeamA(redCardsTeamA);
         displayGoalsForTeamB(goalsTeamB);
         displayYellowCardsForTeamB(yellowCardsTeamB);
         displayRedCardsForTeamB(redCardsTeamB);
+        displayFoulsTeamA(foulsTeamA);
+        displayFoulsTeamB(foulsTeamB);
+        displayOffsidesA(offsidesTeamA);
+        displayOffsidesB(offsidesTeamB);
+        displayCornersA(cornersTeamA);
+        displayCornersB(cornersTeamB);
+        setPossesionTeamZero();
     }
 
 }
